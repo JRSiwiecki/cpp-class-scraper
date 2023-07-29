0070b78495b671c302dc24ba2f42275e6532ab8a
JRSiwiecki <JRSiwiecki@gmail.com>
2023-07-28 21:54:34 -0700

    Add API query of OpenCPP api, refactor code into functions, respond with data sorted by highest GPA

--- scraper.py
+++ scraper.py
@@ -1,94 +1,163 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
-URL = "https://catalog.cpp.edu/preview_program.php?catoid=65&poid=17161"
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
+def scrape_cpp_data():
+    URL = "https://catalog.cpp.edu/preview_program.php?catoid=65&poid=17161"
+    page = requests.get(URL)
 
-class_areas = soup.find_all("div", class_="acalog-core")
+    soup = BeautifulSoup(page.content, "html.parser")
 
-# top element is current area
-area_stack = []
+    global class_areas
+    class_areas = soup.find_all("div", class_="acalog-core")
 
-# top element is current section
-section_stack = []
 
-# contains list of dicts containing sections in that area
-area_map = {}
+def categorize_courses():
+    # top element is current area
+    global area_stack
+    area_stack = []
 
-# contains list of classes in that section
-section_map = {}
+    # top element is current section
+    global section_stack
+    section_stack = []
 
-for class_area in class_areas:
-    area = class_area.find("h2")
-    section = class_area.find("h3")
+    # contains list of dicts containing sections in that area
+    global area_map
+    area_map = {}
 
-    courses = class_area.find_all("li", class_="acalog-course")
+    # contains list of classes in that section
+    global section_map
+    section_map = {}
 
-    if area:
-        # 5th index contains area letter
-        area_stack.insert(0, area.text[5])
-        current_area = area_stack[0]
+    for class_area in class_areas:
+        area = class_area.find("h2")
+        section = class_area.find("h3")
 
-        area_map[current_area] = []
+        courses = class_area.find_all("li", class_="acalog-course")
 
-    if section:
-        if "Note(s)" in section.text:
+        if area:
+            # 5th index contains area letter
+            area_stack.insert(0, area.text[5])
+            current_area = area_stack[0]
+
+            area_map[current_area] = []
+
+        if section:
+            if "Note(s)" in section.text:
+                break
+
+            if "(" in section.text:
+                end_marker = section.text.index("(") - 1
+            else:
+                end_marker = section.text.index(":")
+
+            # 0th index contains section number
+            section_stack.insert(0, section.text[0:end_marker])
+            current_section = section_stack[0]
+
+            section_map[current_section] = []
+            area_map[current_area].append(section.text[0:end_marker])
+
+        for course in courses:
+            spans = course.find_all("span")
+
+            for span in spans:
+                if span:
+                    end_marker = span.text.index("(")
+
+                    section_map[current_section].append(span.text[0 : end_marker - 1])
+
+
+def get_opencpp_api_data():
+    global response
+    response = requests.post("https://cpp-scheduler.herokuapp.com/data/courses/find")
+
+    global json_object
+    json_object = json.loads(response.text)
+
+
+def recommend_course():
+    run = True
+
+    while run:
+        print("Here are the available areas and their sections: \n")
+
+        for area in area_map.keys():
+            print(area)
+            print(area_map[area])
+            print()
+
+        requested_data = input(
+            "Enter the class area + section you would like to search the easiest class for (ex. A1, B2, C3) or enter Q to quit: "
+        )
+
+        if len(requested_data) < 2:
+            print("Input is too short. Try again.")
+            continue
+
+        requested_area = requested_data[0].upper()
+        requested_section = requested_data[1]
+
+        if requested_area.isdigit():
+            print("Area must be a letter. Try again.")
+            continue
+
+        if requested_section.isalpha():
+            print("Section must be a number. Try again.")
+            continue
+
+        if requested_area not in area_map:
+            print("Area does not exist. Try again.")
+            continue
+
+        found_sections = area_map[requested_area]
+
+        found_classes = []
+
+        for section in found_sections:
+            if requested_section in section:
+                found_classes = section_map[section]
+                break
+
+        if not found_classes:
+            print("No sections found. Try again.")
+            continue
+
+        course_codes = []
+
+        for found_class in found_classes:
+            end_marker = found_class.index("-") - 1
+
+            course_codes.append(found_class[0:end_marker])
+
+        course_gpas = []
+
+        for object in json_object:
+            for course_code in course_codes:
+                if course_code in object["Label"]:
+                    if object["AvgGPA"] is None:
+                        course_gpas.append([course_code, 0])
+                        continue
+
+                    course_gpas.append([course_code, round(float(object["AvgGPA"]), 2)])
+
+        course_gpas = sorted(course_gpas, key=lambda x: x[1], reverse=True)
+
+        print("The classes you should take by highest GPA are in this order: \n")
+        for course_gpa in course_gpas:
+            print(course_gpa)
+
+        requested_data = input("\nSearch again? Y/N: ")
+
+        if requested_data.lower() == "n":
+            run = False
             break
-
-        if "(" in section.text:
-            end_marker = section.text.index("(") - 1
         else:
-            end_marker = section.text.index(":")
-
-        # 0th index contains section number
-        section_stack.insert(0, section.text[0:end_marker])
-        current_section = section_stack[0]
-
-        section_map[current_section] = []
-        area_map[current_area].append(section.text[0:end_marker])
-
-    for course in courses:
-        spans = course.find_all("span")
-
-        for span in spans:
-            if span:
-                end_marker = span.text.index("(")
-
-                section_map[current_section].append(span.text[0 : end_marker - 1])
+            continue
 
 
-print("Welcome! Here are the available areas and their sections: \n")
-
-for area in area_map.keys():
-    print(area)
-    print(area_map[area])
-    print()
-
-requested_data = input(
-    "Enter the class area + section you would like to search the easiest class for (ex. A1, B2, C3):"
-)
-
-requested_area = requested_data[0]
-requested_section = requested_data[1]
-
-found_sections = area_map[requested_area]
-
-found_classes = []
-
-for section in found_sections:
-    if requested_section in section:
-        found_classes = section_map[section]
-        break
-
-# print(found_classes)
-
-course_codes = []
-
-for found_class in found_classes:
-    end_marker = found_class.index("-") - 1
-
-    course_codes.append(found_class[0:end_marker])
-
-# print(course_codes)
+scrape_cpp_data()
+categorize_courses()
+get_opencpp_api_data()
+recommend_course()

debcb73cea753126c17929605dc8989d4812bd28
JRSiwiecki <JRSiwiecki@gmail.com>
2023-07-29 00:25:10 -0700

    Add minor front-end input validation, add dark theme, add h1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
+// React Imports
 import React, { useState } from "react";
+
+// MUI Imports
 import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+// CSS Imports
 import "./App.css";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 export default function App() {
   const [year, setYear] = useState("");
   const [areaSection, setAreaSection] = useState("");
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
   async function postJSON(data) {
     try {
@@ -24,41 +38,62 @@ export default function App() {
     }
   }
 
-  function handleClick() {
-    postJSON({ year: year, area_section: areaSection });
-  }
-
   const handleYearChange = (event) => {
     setYear(event.target.value);
+    validateInput(event.target.value, areaSection);
   };
 
   const handleAreaSectionChange = (event) => {
     setAreaSection(event.target.value);
+    validateInput(year, event.target.value);
   };
 
+  const validateInput = (yearValue, areaSectionValue) => {
+    const isValid = yearValue.trim() !== "" && areaSectionValue.trim() !== "";
+    setIsButtonDisabled(!isValid);
+  };
+
+  function handleClick() {
+    if (isButtonDisabled) {
+      return;
+    }
+
+    postJSON({ year: year, area_section: areaSection });
+  }
+
   return (
-    <div className="App">
-      <TextField
-        id="outlined-basic"
-        label="Catalog Year"
-        variant="outlined"
-        required
-        helperText="2021-2023"
-        value={year}
-        onChange={handleYearChange}
-      />
-      <TextField
-        id="outlined-basic"
-        label="Area + Section"
-        variant="outlined"
-        required
-        helperText="Ex) A1, B2, C3, E0, F0"
-        value={areaSection}
-        onChange={handleAreaSectionChange}
-      />
-      <Button variant="contained" onClick={handleClick}>
-        Recommend Course
-      </Button>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div class="App">
+        <h1>General Education Course Recommender</h1>
+        <TextField
+          id="outlined-basic"
+          label="Catalog Year"
+          variant="outlined"
+          required
+          helperText="2021-2023"
+          value={year}
+          onChange={handleYearChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Area + Section"
+          variant="outlined"
+          required
+          helperText="Ex) A1, B2, C3, E0, F0"
+          value={areaSection}
+          onChange={handleAreaSectionChange}
+        />
+        <br />
+        <br />
+        <Button
+          variant="contained"
+          onClick={handleClick}
+          disabled={isButtonDisabled}
+        >
+          Recommend Course
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }

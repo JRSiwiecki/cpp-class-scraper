cbd70ae8f4ea54354c4a2a811e6ff149144d51f9
JRSiwiecki <JRSiwiecki@gmail.com>
2023-07-29 00:13:19 -0700

    Allow front-end to POST back-end with user input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [year, setYear] = useState("");
+  const [areaSection, setAreaSection] = useState("");
+
+  async function postJSON(data) {
+    try {
+      const response = await fetch("/api/recommend", {
+        method: "POST", // or 'PUT'
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      console.log("Success:", result);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  function handleClick() {
+    postJSON({ year: year, area_section: areaSection });
+  }
+
+  const handleYearChange = (event) => {
+    setYear(event.target.value);
+  };
+
+  const handleAreaSectionChange = (event) => {
+    setAreaSection(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TextField
+        id="outlined-basic"
+        label="Catalog Year"
+        variant="outlined"
+        required
+        helperText="2021-2023"
+        value={year}
+        onChange={handleYearChange}
+      />
+      <TextField
+        id="outlined-basic"
+        label="Area + Section"
+        variant="outlined"
+        required
+        helperText="Ex) A1, B2, C3, E0, F0"
+        value={areaSection}
+        onChange={handleAreaSectionChange}
+      />
+      <Button variant="contained" onClick={handleClick}>
+        Recommend Course
+      </Button>
     </div>
   );
 }
-
-export default App;

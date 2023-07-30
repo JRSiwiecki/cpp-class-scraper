fd69c17815f3f581c48b5d6c587e7e30b0daa223
JRSiwiecki <JRSiwiecki@gmail.com>
2023-07-29 22:30:02 -0700

    Add instructions to home page and styling for instructions

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,6 +6,19 @@ export default function Home() {
     <div class="home-container">
       <Links />
       <h1>Home</h1>
+      <h2>Instructions for using Course Recommender</h2>
+      <div class="instructions-container">
+        <p>1. Go to Course Recommender</p>
+        <p>
+          2. Input your catalog year (the year you started at CPP, currently
+          only 2021 to 2023)
+        </p>
+        <p>
+          3. Input the general education section you would like to be
+          recommended courses for
+        </p>
+        <p>4. Click recommend courses</p>
+      </div>
     </div>
   );
 }

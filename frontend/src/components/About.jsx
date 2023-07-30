027dbbd9cf282fa9d9c12feffb1d781775c87c0b
JRSiwiecki <JRSiwiecki@gmail.com>
2023-07-30 16:55:42 -0700

    Fix possible tabnabbing exploit with links on About page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -13,7 +13,11 @@ export default function About() {
         <h1>Creator</h1>
         <p>Made by Joseph Siwiecki</p>
 
-        <a href="https://www.linkedin.com/in/josephsiwiecki/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/josephsiwiecki/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           LinkedIn
         </a>
 
@@ -22,19 +26,24 @@ export default function About() {
         <a
           href="https://github.com/JRSiwiecki/cpp-ge-recommender"
           target="_blank"
+          rel="noopener noreferrer"
         >
           Github Repository
         </a>
 
         <h2>Frameworks & Libraries</h2>
 
-        <a href="https://react.dev/" target="_blank">
+        <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
           React (Front-end + Router)
         </a>
 
         <br />
 
-        <a href="https://flask.palletsprojects.com/en/2.3.x/" target="_blank">
+        <a
+          href="https://flask.palletsprojects.com/en/2.3.x/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Flask (Server, Scraper)
         </a>
 
@@ -43,6 +52,7 @@ export default function About() {
         <a
           href="https://www.crummy.com/software/BeautifulSoup/"
           target="_blank"
+          rel="noopener noreferrer"
         >
           BeautifulSoup (Scraper)
         </a>
@@ -52,6 +62,7 @@ export default function About() {
         <a
           href="https://catalog.cpp.edu/preview_program.php?catoid=53&poid=13914"
           target="_blank"
+          rel="noopener noreferrer"
         >
           California State Polytechnic University, Pomona - General Education
           Course List
@@ -59,7 +70,11 @@ export default function About() {
 
         <br />
 
-        <a href="https://github.com/ZombiMigz/opencpp-api" target="_blank">
+        <a
+          href="https://github.com/ZombiMigz/opencpp-api"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           ZombiMigz - OpenCPP API
         </a>
 
@@ -68,6 +83,7 @@ export default function About() {
         <a
           href="https://www.flaticon.com/free-icons/education"
           target="_blank"
+          rel="noopener noreferrer"
           title="education icons"
         >
           Education icons created by Freepik - Flaticon

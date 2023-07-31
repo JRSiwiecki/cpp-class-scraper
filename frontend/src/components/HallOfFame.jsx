d8b1b9649b623767e083ae2acd55e78ba2016b41
JRSiwiecki <JRSiwiecki@gmail.com>
2023-07-30 21:58:37 -0700

    Add basic grid styling for hall of fame component

--- frontend/src/components/HallOfFame.jsx
+++ frontend/src/components/HallOfFame.jsx
@@ -1,7 +1,34 @@
+import React, { useEffect } from "react";
+
 export default function HallOfFame() {
+  const courseAreaSections = [
+    "A1",
+    "A2",
+    "A3",
+    "B1",
+    "B2",
+    "B4",
+    "B5",
+    "C1",
+    "C2",
+    "C3",
+    "D1",
+    "D2",
+    "D4",
+    "E",
+    "F",
+  ];
+
   return (
-    <main>
-      <p>Hall of Fame</p>
-    </main>
+    <div class="hall-of-fame-container">
+      <h1>Hall of Fame</h1>
+      <main>
+        {courseAreaSections.map((areaSection, index) => (
+          <section key={index} class="area-section-container">
+            {areaSection}
+          </section>
+        ))}
+      </main>
+    </div>
   );
 }

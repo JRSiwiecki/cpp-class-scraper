4717a665d404ab6040551a3d86ca664413a2242a
JRSiwiecki <JRSiwiecki@gmail.com>
2023-09-18 17:03:15 -0700

    Re-work about + credits page to add BDM and CPPScheduler :)

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -10,95 +10,136 @@ export default function About() {
           <h1>About + Credits</h1>
         </header>
 
-        <h1>Creator</h1>
-        <p>Made by Joseph Siwiecki</p>
+        <section>
+          <h1>Creator</h1>
+          <p>Made by Joseph Siwiecki</p>
 
-        <a
-          href="https://www.linkedin.com/in/josephsiwiecki/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
+          <a
+            href="https://www.linkedin.com/in/josephsiwiecki/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+
+          <br />
+
+          <a
+            href="https://github.com/JRSiwiecki/cpp-ge-recommender"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github Repository
+          </a>
+        </section>
+
+        <section>
+          <h2>Frameworks & Libraries</h2>
+
+          <a
+            href="https://react.dev/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React (Front-end + Router)
+          </a>
+
+          <br />
+
+          <a href="https://mui.com/" target="_blank" rel="noopener noreferrer">
+            Material UI (Front-end)
+          </a>
+
+          <br />
+
+          <a
+            href="https://flask.palletsprojects.com/en/2.3.x/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Flask (Server)
+          </a>
+
+          <br />
+
+          <a
+            href="https://www.crummy.com/software/BeautifulSoup/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            BeautifulSoup (Scraper)
+          </a>
+        </section>
+
+        <section>
+          <h2>Data</h2>
+
+          <a
+            href="https://catalog.cpp.edu/preview_program.php?catoid=53&poid=13914"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            California State Polytechnic University, Pomona - General Education
+            Course List
+          </a>
+
+          <br />
+
+          <a
+            href="https://github.com/ZombiMigz/opencpp-api"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ZombiMigz - OpenCPP API
+          </a>
+        </section>
+
+        <section>
+          <h2>Other Helpful Applications</h2>
+
+          <br />
+
+          <a
+            href="https://www.cppscheduler.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ZombiMigz - CPPScheduler.com
+          </a>
+
+          <br />
+
+          <a
+            href="https://broncodirect.me/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CPP CS Discord - BroncoDirectMe Extension
+          </a>
+        </section>
 
         <br />
-
-        <a
-          href="https://github.com/JRSiwiecki/cpp-ge-recommender"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Repository
-        </a>
-
-        <h2>Frameworks & Libraries</h2>
-
-        <a href="https://react.dev/" target="_blank" rel="noopener noreferrer">
-          React (Front-end + Router)
-        </a>
-
-        <br />
-
-        <a href="https://mui.com/" target="_blank" rel="noopener noreferrer">
-          Material UI (Front-end)
-        </a>
-
-        <br />
-
-        <a
-          href="https://flask.palletsprojects.com/en/2.3.x/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Flask (Server)
-        </a>
-
-        <br />
-
-        <a
-          href="https://www.crummy.com/software/BeautifulSoup/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          BeautifulSoup (Scraper)
-        </a>
-
-        <h2>Data</h2>
-
-        <a
-          href="https://catalog.cpp.edu/preview_program.php?catoid=53&poid=13914"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          California State Polytechnic University, Pomona - General Education
-          Course List
-        </a>
-
-        <br />
-
-        <a
-          href="https://github.com/ZombiMigz/opencpp-api"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          ZombiMigz - OpenCPP API
-        </a>
-
-        <br />
-
-        <a
-          href="https://www.flaticon.com/free-icons/education"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="education icons"
-        >
-          Education icons created by Freepik - Flaticon
-        </a>
-
-        <br />
-
-        <p>Not affiliated with CPP.</p>
       </main>
+
+      <footer className="about-container">
+        <section>
+          <h3>Icons</h3>
+          <a
+            href="https://www.flaticon.com/free-icons/education"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="education icons"
+          >
+            Education icons created by Freepik - Flaticon
+          </a>
+        </section>
+
+        <br />
+
+        <section>
+          <p>Not affiliated with CPP.</p>
+        </section>
+      </footer>
     </div>
   );
 }
